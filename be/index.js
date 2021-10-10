0ad9dc6b1f4e7cf43c4eb812854385b5003f7e25
Adam Berg <adam@xyzdigital.com>
2021-10-10 14:22:38 -0700

    Updat eport to 3003

--- be/index.js
+++ be/index.js
@@ -181,6 +181,6 @@ app.get("*", function(req, res, next) {
   res.sendFile(path.resolve("../vanilla/index.html"));
 })
 
-app.listen(3001, function () {
-  console.log("Server listening on port 3001.");
+app.listen(3003, function () {
+  console.log("Server listening on port 3003.");
 });
